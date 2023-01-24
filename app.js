3ee888fc4b0be40ce0975f3abea27edf908442da
Xu Che <xuche1998@gmail.com>
2023-01-24 21:39:20 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controller/blogs')
+
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+
+app.use(middleware.errorHandler)
+
+module.exports = app
